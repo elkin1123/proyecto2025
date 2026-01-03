338003803d0f05fc4baafe962eb8ef9d31ec78bd
elkin1123 <delgadolopezelkinjoshua61@gmail.com>
2026-01-02 19:05:50 -0500

    fix build.sh shebang

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env hash
-#Exit on error 
-set -e errexit 
+#!/usr/bin/env bash
+# Exit on error
+set -e errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
+# Install dependencies
 pip install -r requirements.txt
 
-# Convert static asset files
+# Collect static files
 python manage.py collectstatic --no-input
 
-# Apply any outstanding database migrations
+# Apply database migrations
 python manage.py migrate
